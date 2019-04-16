a646da132afc86d694e5375574a7b895d0abc6c1
yf.feng <yf.feng@shunwang.com>
2019-04-16 21:34:15 +0800

    判断胜利条件

--- Gobang.js
+++ Gobang.js
@@ -15,12 +15,94 @@ class Game {
             // order: 1 2 3 4 ……
             this._board.push({ type: "empty", order: 0 });
         }
+
+        this._board[0] = { type: "black", order: 0 };
+        this._board[6] = { type: "black", order: 0 };
+        this._board[12] = { type: "black", order: 0 };
+        this._board[18] = { type: "black", order: 0 };
+        this._board[24] = { type: "black", order: 0 };
+        this._board[30] = { type: "black", order: 0 };
+        console.log(this.CheckWin("black", { x: 1, y: 0 }));
     }
 
     IndexToPos(idx) {
         let x = Math.floor(idx / BoardSize);
         let y = Math.floor(idx % BoardSize);
         return { x: x, y: y };
+    }
+
+    PosToIndex(pos) {
+        return pos.x * BoardSize + pos.y;
+    }
+
+    CheckWin(type, pos) {
+        if (this.N_S(type, pos)) {
+            return true;
+        }
+        return false;
+    }
+
+    N_S(type, pos) {
+        let startX = Math.max(0, pos.x - WinCount + 1);
+        let endX = Math.min(BoardSize - 1, pos.x + WinCount);
+        let nWinCount = 0;
+        for (let n = startX; n < endX; n++) {
+            let p = { x: n, y: pos.y };
+            let idx = this.PosToIndex(p);
+            if (this._board[idx].type === type) {
+                nWinCount++;
+            } else {
+                nWinCount = 0;
+            }
+            if (nWinCount === WinCount) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    W_E(type, pos) {
+        let startY = Math.max(0, pos.y - WinCount + 1);
+        let endY = Math.min(BoardSize - 1, pos.y + WinCount);
+        let nWinCount = 0;
+        for (let n = startY; n < endY; n++) {
+            let p = { x: pos.x, y: n };
+            let idx = this.PosToIndex(p);
+            if (this._board[idx].type === type) {
+                nWinCount++;
+            } else {
+                nWinCount = 0;
+            }
+            if (nWinCount === WinCount) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    WN_ES(type, pos) {
+        let StartX = Math.max(0, pos.x - WinCount + 1);
+        let startY = Math.max(0, pos.y - WinCount + 1);
+        let endX = Math.min(BoardSize - 1, pos.x + WinCount);
+        let endY = Math.min(BoardSize - 1, pos.y + WinCount);
+        let nWinCount = 0;
+        for (let n = startY; n < endY; n++) {
+            let p = { x: pos.x, y: n };
+            let idx = this.PosToIndex(p);
+            if (this._board[idx].type === type) {
+                nWinCount++;
+            } else {
+                nWinCount = 0;
+            }
+            if (nWinCount === WinCount) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    EN_WS(type, pos) {
+
     }
 };
 
--- app.js
+++ app.js
@@ -1,71 +1,71 @@
-let NeuralNetwork = require(`./NeuralNetwork.js`);
-let Gobang = new NeuralNetwork(2, 16, 1);
-let samples = [
-    { x: [1.0e-1, 2.0e-1], y: [0] },
-    { x: [1.5e-1, 3.0e-1], y: [0] },
-    { x: [1.6e-1, 3.2e-1], y: [0] },
-    { x: [2.3e-1, 4.6e-1], y: [0] },
-    { x: [3.0e-1, 6.0e-1], y: [0] },
-    { x: [3.1e-1, 6.2e-1], y: [0] },
-    { x: [3.2e-1, 6.4e-1], y: [0] },
-    { x: [3.3e-1, 6.6e-1], y: [0] },
-    { x: [3.4e-1, 6.8e-1], y: [0] },
-    { x: [3.6e-1, 7.2e-1], y: [0] },
-    { x: [3.7e-1, 7.4e-1], y: [0] },
-    { x: [3.8e-1, 7.6e-1], y: [0] },
-    { x: [3.9e-1, 7.8e-1], y: [0] },
-    { x: [1.0e-2, 2.0e-2], y: [0] },
-    { x: [1.0e-3, 2.0e-3], y: [0] },
-    { x: [1.0e-4, 2.0e-4], y: [0] },
-    { x: [5.00e-1, 10.0e-1], y: [0] },
-    { x: [1.55e-1, 3.10e-1], y: [0] },
-    { x: [2.32e-1, 4.64e-1], y: [0] },
-];
+// let NeuralNetwork = require(`./NeuralNetwork.js`);
+// let Gobang = new NeuralNetwork(2, 16, 1);
+// let samples = [
+//     { x: [1.0e-1, 2.0e-1], y: [0] },
+//     { x: [1.5e-1, 3.0e-1], y: [0] },
+//     { x: [1.6e-1, 3.2e-1], y: [0] },
+//     { x: [2.3e-1, 4.6e-1], y: [0] },
+//     { x: [3.0e-1, 6.0e-1], y: [0] },
+//     { x: [3.1e-1, 6.2e-1], y: [0] },
+//     { x: [3.2e-1, 6.4e-1], y: [0] },
+//     { x: [3.3e-1, 6.6e-1], y: [0] },
+//     { x: [3.4e-1, 6.8e-1], y: [0] },
+//     { x: [3.6e-1, 7.2e-1], y: [0] },
+//     { x: [3.7e-1, 7.4e-1], y: [0] },
+//     { x: [3.8e-1, 7.6e-1], y: [0] },
+//     { x: [3.9e-1, 7.8e-1], y: [0] },
+//     { x: [1.0e-2, 2.0e-2], y: [0] },
+//     { x: [1.0e-3, 2.0e-3], y: [0] },
+//     { x: [1.0e-4, 2.0e-4], y: [0] },
+//     { x: [5.00e-1, 10.0e-1], y: [0] },
+//     { x: [1.55e-1, 3.10e-1], y: [0] },
+//     { x: [2.32e-1, 4.64e-1], y: [0] },
+// ];
 
-function minibatch(samples) {
-    samples.forEach(sample => {
-        Gobang.Inputs = sample.x;
-        Gobang.Outputs = sample.y;
-        Gobang.FeedForward();
-        Gobang.Backprop();
-    });
-    let batchsize = samples.length;
-    Gobang.UpdateNabla(batchsize);
-}
+// function minibatch(samples) {
+//     samples.forEach(sample => {
+//         Gobang.Inputs = sample.x;
+//         Gobang.Outputs = sample.y;
+//         Gobang.FeedForward();
+//         Gobang.Backprop();
+//     });
+//     let batchsize = samples.length;
+//     Gobang.UpdateNabla(batchsize);
+// }
 
-let testsamples = [
-    { x: [1.0e-1, 2.0e-1], y: [0] },
-    { x: [1.5e-1, 3.0e-1], y: [1] },
-    { x: [4.1e-1, 8.2e-1], y: [0] },
-    { x: [1.1e-5, 2.2e-5], y: [0] },
-    { x: [3.5e-1, 7.0e-1], y: [0] },
-    { x: [3.5e-1, 7.1e-1], y: [0] },
-    { x: [3.5e-1, 7.2e-1], y: [0] },
-    { x: [3.5e-1, 7.0e-1], y: [0.1] },
-];
+// let testsamples = [
+//     { x: [1.0e-1, 2.0e-1], y: [0] },
+//     { x: [1.5e-1, 3.0e-1], y: [1] },
+//     { x: [4.1e-1, 8.2e-1], y: [0] },
+//     { x: [1.1e-5, 2.2e-5], y: [0] },
+//     { x: [3.5e-1, 7.0e-1], y: [0] },
+//     { x: [3.5e-1, 7.1e-1], y: [0] },
+//     { x: [3.5e-1, 7.2e-1], y: [0] },
+//     { x: [3.5e-1, 7.0e-1], y: [0.1] },
+// ];
 
-function testNetwork(samples) {
-    console.log("test samples:");
-    samples.forEach(sample => {
-        Gobang.Inputs = sample.x;
-        Gobang.Outputs = sample.y;
-        Gobang.FeedForward();
-        Gobang.SimplePrint();
-    });
-}
+// function testNetwork(samples) {
+//     console.log("test samples:");
+//     samples.forEach(sample => {
+//         Gobang.Inputs = sample.x;
+//         Gobang.Outputs = sample.y;
+//         Gobang.FeedForward();
+//         Gobang.SimplePrint();
+//     });
+// }
 
-// using mini-batch stochastic gradient descent
-testNetwork(testsamples);
-for (let n = 0; n < 10000; n++) {
-    let temps = [];
-    for (let s = 0; s < 4; s++) {
-        let randomIdx = Math.floor(Math.random() * samples.length);
-        temps.push(samples[randomIdx]);
-    }
-    //minibatch(samples);
-    minibatch(temps);
-}
-testNetwork(testsamples);
+// // using mini-batch stochastic gradient descent
+// testNetwork(testsamples);
+// for (let n = 0; n < 10000; n++) {
+//     let temps = [];
+//     for (let s = 0; s < 4; s++) {
+//         let randomIdx = Math.floor(Math.random() * samples.length);
+//         temps.push(samples[randomIdx]);
+//     }
+//     //minibatch(samples);
+//     minibatch(temps);
+// }
+// testNetwork(testsamples);
 
 let Game = require(`./Gobang`);
 let GobangGame = new Game;
