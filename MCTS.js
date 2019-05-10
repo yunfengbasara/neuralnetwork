8709ef071208e7f75e6ae19412c9aa781f013950
yf.feng <yf.feng@shunwang.com>
2019-05-10 18:40:20 +0800

    调整

--- MCTS.js
+++ MCTS.js
@@ -8,34 +8,42 @@ class Node {
         this._total = 0;
         this._index = -1;
         this._parent = null;
-        this._children = [];      
-        this._unExpands = []; 
+        this._children = [];
+        this._unExpands = [];
     }
 
     get Win() { return this._win; }
     get Total() { return this._total; }
     get Index() { return this._index; }
-    get Parent() {return this._parent;}
+    get Parent() { return this._parent; }
     get Children() { return this._children; }
-    get UnExpands() {return this._unExpands;}
+    get UnExpands() { return this._unExpands; }
 
     set Win(v) { this._win = v; }
     set Total(v) { this._total = v; }
     set Index(v) { this._index = v; }
-    set Parent(v) {this._parent = v;}
+    set Parent(v) { this._parent = v; }
     set Children(v) { this._children = v; }
-    set UnExpands(v) {this._unExpands = v;}
+    set UnExpands(v) { this._unExpands = v; }
 
     UCB() {
-        if(this._total === 0 || 
-            this._parent === null || 
+        if (this._total === 0 ||
+            this._parent === null ||
             this._parent.Total === 0) {
             return Math.random() * 1e5;
-        } 
+        }
 
         let winRate = this._win / this._total;
-        let lambda =  Math.sqrt(this._parent.Total / this._total);
-        return  winRate + Confident * lambda;
+        let lambda = Math.sqrt(this._parent.Total / this._total);
+        return winRate + Confident * lambda;
+    }
+
+    Value() {
+        if (this._total === 0) {
+            return 0;
+        }
+
+        return this._win / this._total;
     }
 
     Selection() {
@@ -47,10 +55,24 @@ class Node {
         let selecNode = {};
         this._children.forEach(n => {
             let ucb = n.UCB();
-            if ( ucb < maxUCB) {
+            if (ucb < maxUCB) {
                 return;
             }
             maxUCB = ucb;
+            selecNode = n;
+        });
+        return selecNode;
+    }
+
+    SelectMaxValue() {
+        let maxValue = 0;
+        let selecNode = {};
+        this._children.forEach(n => {
+            let value = n.Value();
+            if (value < maxValue) {
+                return;
+            }
+            maxValue = value;
             selecNode = n;
         });
         return selecNode;
@@ -72,56 +94,79 @@ class Node {
         let childrenIndex = this._children.map(c => c.Index);
         return unExpands.concat(childrenIndex);
     }
-} 
-    
+}
+
 // Monte Carlo Tree Search with UCB
 class MCTS {
     constructor() {
         this._root = {};
-        this._simulationWin = 0;
+        this._nextWin = false;  // 隔层设置胜负
     }
 
     Init(size) {
         this._root = new Node();
-        for(let index = 0; index < size; index++) {
+        for (let index = 0; index < size; index++) {
             this._root.UnExpands.push(index);
         }
     }
 
     Run() {
-        let node = this.Selection();
+        let { node, line } = this.Selection();
         this.Simulation(node);
         this.Backpropagation(node);
     }
 
+    SelectMaxValue() {
+        return this._root.SelectMaxValue();
+    }
+
     Selection() {
+        let line = [];
         let node = this._root.Selection();
+        line.push(node.Index);
         while (node.Total !== 0) {
             node = node.Selection();
+            line.push(node.Index);
         }
-        return node;
+        return { node, line };
     }
 
     Simulation(node) {
-        this._simulationWin = Math.floor(Math.random() * 2);
-        node.Win += this._simulationWin;
         node.Total++;
+        if (Math.floor(Math.random() * 2)) {
+            this._nextWin = false;
+            node.Win++;
+        } else {
+            this._nextWin = true;
+        }
     }
 
     Backpropagation(node) {
         let parent = node.Parent;
         while (parent !== null) {
             parent.Total++;
-            parent.Win += this._simulationWin;
+            this._nextWin ? 0 : parent.Win++;
+            this._nextWin = !this._nextWin;
             parent = parent.Parent;
         }
     }
 
     Print() {
-        let expands = this._root.Children.map(n => n.Index);
-        let unexpands = this._root.UnExpands.slice();
-        console.log(`expands:${expands}`);
-        console.log(`unexpands:${unexpands}`);
+        // let { node, line } = this.Selection();
+        // let expands = node.Children.map(n => n.Index);
+        // let unexpands = node.UnExpands.slice();
+        // console.log(`expands:${expands}`);
+        // console.log(`unexpands:${unexpands}`);
+        // console.log(`line:${line}`);
+        // console.log(`index:${node.Index}`);
+        // console.log(`win/total:${node.Win}/${node.Total}`);
+
+        // let parent = node.Parent;
+        // while (parent !== null) {
+        //     console.log(`index:${parent.Index}`);
+        //     console.log(`win/total:${parent.Win}/${parent.Total}`);
+        //     parent = parent.Parent;
+        // }
     }
 }
 
